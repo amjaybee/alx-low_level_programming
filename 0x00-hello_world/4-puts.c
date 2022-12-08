c6a5a6844323e2ae979096f5bdb65b28e559dfd5
Your Name <you@example.com>
2022-12-08 15:32:34 -0800

    changes made

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
-
-int main(void)
+/**
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success
+ */
 {
 	puts("Programming is like building a multilingual puzzle\n");
 	return (0);

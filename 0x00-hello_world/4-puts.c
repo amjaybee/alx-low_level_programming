bd1074e09bd77a39c0b439cdf7a3e9fd69535d61
Your Name <you@example.com>
2022-12-08 15:40:03 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * Description: main Prints out a string to stout
+ * Return: 0 if success
+ */
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-My readme file
+on: main - Prints out a string to stdout.
+ * Return: 0 if success
+ */My readme file

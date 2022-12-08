34d55e1c965ce1d976881b57c0f71e66a120cc9f
Your Name <you@example.com>
2022-12-08 15:34:40 -0800

    chnages made

--- 0x00-hello_world/4-puts.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-/**
- * Description: main - Prints out a string to stdout.
- * Return: 0 if success
- */
-{
-	puts("Programming is like building a multilingual puzzle\n");
-	return (0);
-}

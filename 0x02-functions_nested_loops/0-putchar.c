6e227f50c5fd65bf619a5178a7acfe4f79b5e6b1
Your Name <you@example.com>
2022-12-13 09:09:17 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* main - Entry point
+* 
+* Return: Always 0
+*/
+int main(void)
+{
+char jaybee[9] = "_putchar";
+int i = 0;
+while (i < 8)
+{
+_putchar(jaybee[i]);
+i++;
+}
+_putchar('\n');
+return (0);
+}

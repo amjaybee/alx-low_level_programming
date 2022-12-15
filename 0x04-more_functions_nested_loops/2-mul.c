5a039998800e4bb3968a5f272b784c5350fe8a54
Your Name <you@example.com>
2022-12-15 03:06:50 -0800

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+nclude "main.h"
+/**
+ * mul - returns the product of its parameters
+ * @a: int type numbers
+ * @b: int type number
+ * Return: 0
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
